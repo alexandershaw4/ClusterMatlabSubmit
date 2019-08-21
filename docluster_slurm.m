e6a3bdcaafaa5f8e8327966f2d3859fb60bde42f
Alex <alexandershaw4@gmail.com>
2019-08-21 15:48:57 +0100

    fixed numeric inputs

--- docluster_slurm.m
+++ docluster_slurm.m
@@ -12,17 +12,23 @@ fh  = which(f);
 fh  = [fileparts(fh) '/'];
 
 ln{1} = sprintf('#!/bin/bash\n');
+ln{1} = [ln{1} '#SBATCH --job-name=aOPTIM'];
 ln{2} = sprintf('\n');
 ln{2} = sprintf([ln{2} 'cd ' h '\n']); 
-ln{3} = sprintf('\nmatlab -nodesktop -nosplash -r "');
+ln{3} = sprintf('\nmatlab_2017b -nodesktop -nosplash -r "');
 ln{3} = [ln{3} ' addpath ' fh '; '];
 ln{3} = [ln{3} f];
 
 
-try varargin{1}; catch varargin{1} = []; end
+%try varargin{1}; catch varargin{1} = []; end
 
 if any(varargin{1});
-    ln{4} = ['(''' varargin{1}];
+    
+    if isnumeric(varargin{1})
+        ln{4} =  ['(' num2str(varargin{1})];
+    else
+        ln{4} = ['(''' varargin{1}];
+    end
     
     if length(varargin) > 1
         for j = 2:length(varargin)
@@ -30,25 +36,32 @@ if any(varargin{1});
         end
     end
     
-    ln{4} = [ln{4} ''');exit;"'];
-
+    % end input string 
+    if isnumeric(varargin{end})
+        ln{4} = [ln{4} ');exit;"'];
+    else
+        ln{4} = [ln{4} ''');exit;"'];
+    end
 else
     ln{4} = [';exit"'];
 end
 
 cmd = strcat(ln{1},ln{2},ln{3}, ln{4});
 
-dlmwrite(['job_' date '.sh'],cmd,'delimiter','');
-unix(['chmod a+x job_' date '.sh']) ;
+id = strrep(strrep(datestr(now),' ','_'),'-','_');
+
+dlmwrite(['job_' id '.sh'],cmd,'delimiter','');
+unix(['chmod a+x job_' id '.sh']) ;
 
 
 if any(varargin{1})
     inp = [];
     for k = 1:length(varargin)
         if k == 1; inp = [inp ' ']; end
-        try   inp = [inp varargin{k} '=' evalinContext(varargin{k}) ];
-        catch inp = [inp '''' varargin{k} ''''];
-        end
+        %try   
+            %inp = [inp varargin{k} '=' evalinContext(varargin{k}) ];
+        %catch inp = [inp '''' varargin{k} ''''];
+        %end
         if k ~= length(varargin)
             inp = [inp ',' ];
         end
@@ -58,5 +71,5 @@ else
     exstr{1} = 'sbatch ';
 end
 
-torun = [exstr{:} ' job_' date '.sh'];
-unix(torun)
+torun = [exstr{:} ' job_' id '.sh'];
+unix(torun); pause(1);
